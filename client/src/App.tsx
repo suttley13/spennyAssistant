18ee4579fe9ee335ec60b70d5d8cc82f58894e0d
suttley13 <spencer.uttley@gmail.com>
2025-03-16 08:49:12 -0500

    Change app name from PM.AI to Spenny Assistant

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -93,7 +93,7 @@ const App: React.FC = () => {
         <Header>
           <Logo>
             <GlitterIcon />
-            PM.AI
+            Spenny Assistant
           </Logo>
           <StorageToggle />
           <HeaderActions>
